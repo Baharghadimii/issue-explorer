62ccc929a16958c8ab336febac6daee31d059ba1
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-30 13:26:33 -0800

    add transitions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.scss';
 import SearchBar from "./components/SearchPage";
 import Table from './components/Table';
 import useApplicationData from './hooks/useApplicationData';
+import useVisualMode from './hooks/useVisualMode';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 let DATA = [];
+const EMPTY = 'EMPTY';
+const SPINNER = 'SPINNER';
+const SHOW = 'SHOW';
 
 function App() {
 
@@ -12,6 +18,9 @@ function App() {
   const [state, setState] = useState({
     list: []
   });
+  const { mode, transition } = useVisualMode(
+    state.list ? SHOW : EMPTY
+  );
 
   const select = (value) => {
     if (value === 'open') {
@@ -28,9 +37,11 @@ function App() {
     }
   }
   const search = (data) => {
+    transition(SPINNER);
     const requestData = data.split('com/');
     setList(requestData[1]).then((data) => {
       setState({ list: data.list });
+      transition(SHOW);
       DATA = data.list;
     });
 
@@ -39,9 +50,11 @@ function App() {
   return (
     <div className="App">
       <SearchBar onSearch={search} />
-      <div className="tables">
+      {mode === 'EMPTY' && <h1>Place the repository link in the search bar and click to see the issues!</h1>}
+      {mode === 'SHOW' && <div className="tables">
         <Table data={state} select={select} />
-      </div>
+      </div>}
+      {mode === 'SPINNER' && <CircularProgress style={{ marginTop: '5rem' }} disableShrink />}
     </div>
 
   );
